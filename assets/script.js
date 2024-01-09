39966a36b62cecd35d9fab48a0215f5eb2f1c26c
Alexandre TAHI <62557152+alexxtahi@users.noreply.github.com>
2024-01-09 21:50:58 +0100

    Symfony version

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-function fillNameInputs(nom, prenom) {
-    const lastname = document.getElementById("nom");
-    const firstname = document.getElementById("prenom");
-    lastname.value = nom;
-    firstname.value = prenom;
-}
